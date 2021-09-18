3dd96f5ca5a0a796be6fb1c73784431d0e51c38a
Ramiro <rprieto@aplay.com.ar>
2021-09-18 14:49:03 -0300

    arreglos

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 
-const skills = document.getElementById("skills");
-const quests = document.getElementById('quests');
-const inventory = document.getElementById('inventory');
-const blueprint = document.getElementById('blueprint');
+// const skills = document.getElementById("skills");
+// const quests = document.getElementById('quests');
+// const inventory = document.getElementById('inventory');
+// const blueprint = document.getElementById('blueprint');
 
 const skills_p = document.getElementById('pantalla__skills');
 const quests_p = document.getElementById('pantalla__quests');
 const default_p = document.getElementById('pantalla__default');
+const map_p = document.getElementById('pantalla__map');
+
 
 
 function selected(selected){
@@ -16,23 +18,39 @@ function selected(selected){
         skills_p.classList.add('block');
         quests_p.classList.add('none');
         default_p.classList.add('none');
+        map_p.classList.add('none')
         skills_p.classList.remove('none');
         quests_p.classList.remove('block');
         default_p.classList.remove('block');
+        map_p.classList.remove('block');
     }else if(selected == "quests"){
         skills_p.classList.add('none');
         quests_p.classList.add('block');
         default_p.classList.add('none');
+        map_p.classList.add('none');
         skills_p.classList.remove('block');
         quests_p.classList.remove('none');
         default_p.classList.remove('block');
-    }else{
+        map_p.classList.remove('block');
+    }else if(selected == "map"){
+        skills_p.classList.add('none');
+        quests_p.classList.add('none');
+        default_p.classList.add('none');
+        map_p.classList.add('block');
+        skills_p.classList.remove('block');
+        quests_p.classList.remove('block');
+        default_p.classList.remove('block');
+        map_p.classList.remove('none');
+    }
+    else{
         skills_p.classList.add('none');
         quests_p.classList.add('none');
         default_p.classList.add('block');
+        map_p.classList.add('none');
         skills_p.classList.remove('block');
         quests_p.classList.remove('block');
         default_p.classList.remove('none');
+        map_p.classList.remove('block');
     }
 }
 
