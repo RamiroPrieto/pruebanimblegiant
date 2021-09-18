aeaaa6300e34f767dd7d6b66058b6a869511811f
Ramiro <rprieto@aplay.com.ar>
2021-09-18 18:39:09 -0300

    arreglos

--- scripts.js
+++ scripts.js
@@ -56,3 +56,16 @@ function selected(selected){
 
 
 
+// function circulo(numero){
+//     const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+
+//     array.forEach(valor){
+//         if(valor == numero){
+//             const si = document.getElementById(numero);
+//             si.classList.add('block');
+//         }else{
+//             const no = document.getElementById(numero);
+//             no.classList.add('none');
+//         }
+//     }
+// }
